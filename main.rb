25d037851df0ba189e0d70352e156d41101fde00
Carl Wiedemann <carl.wiedemann@checkr.com>
2022-09-10 14:30:53 -0600

    Ok now we will try polymorphism

--- main.rb
+++ main.rb
@@ -11,8 +11,15 @@ class Author
   field :first_name, type: String
   field :last_name, type: String
 
+  # Let's look at using relations in different collections.
   # Adding 'dependent means it will be destroyed if it is abandoned'
   has_many :addresses, dependent: :destroy
+
+  # Alternatively, how about relations in the same collection
+  # embeds_many :addresses
+
+  # How about tags?
+  has_and_belongs_to_many :tags
 end
 
 class Address
@@ -25,7 +32,23 @@ class Address
   field :postal_code, type: String
   field :country, type: String
 
+  # For different collections.
   belongs_to :author
+
+  # For same collection
+  # embedded_in :author
+
+  # How about tags?
+  has_and_belongs_to_many :tags
+end
+
+class Tag
+  include Mongoid::Document
+
+  field :name, type: String
+
+  has_and_belongs_to_many :authors
+  has_and_belongs_to_many :addresses
 end
 
 ##
@@ -35,7 +58,8 @@ lambda do
 
   # Cleanup.
   Author.destroy_all
-  # Address.destroy_all
+  Address.destroy_all
+  Tag.destroy_all
 
   ############
   # EXERCISE #
@@ -45,7 +69,14 @@ lambda do
   my_author.first_name = 'Martin'
   my_author.last_name = 'Fowler'
 
+  my_first_tag = Tag.new
+  my_first_tag.name = 'apple'
+  my_first_tag.save!
+
+  my_author.tags << my_first_tag
   my_author.save!
+
+  pp my_author
 
   ############
   # EXERCISE #
@@ -58,34 +89,21 @@ lambda do
   my_first_address.postal_code = '80917'
   my_first_address.country = 'US'
 
-  # Does this need to be saved first?
-  # my_first_address.save!
+  my_second_tag = Tag.new
+  my_second_tag.name = 'orange'
+  my_second_tag.save!
+
+  my_first_address.tags << my_second_tag
 
   my_author.addresses = [
     my_first_address
   ]
 
+  # This appears to save everything.
   my_author.save!
 
   # Observe the relations.
   pp my_author.addresses
   pp my_first_address.author
-
-  ############
-  # EXERCISE #
-  ############
-  # Ok, let's actually see the persistence behavior that we expect.
-  my_second_address = Address.new
-  my_second_address.street_primary = '2233 Collegiate Dr'
-  my_second_address.city = 'Colorado Springs'
-  my_second_address.province = 'Colorado'
-  my_second_address.postal_code = '80918'
-  my_second_address.country = 'US'
-
-  # THIS ACTUALLY PERSISTS INFORMATION!!!!!!!!! RAGE RAGE RAGE
-  my_author.addresses = [
-    my_second_address
-  ]
-
 
 end.call
